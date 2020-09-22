3497c6be4d9288b111d34b04ab72457efa1ef994
Siyamswe <42336409+Siyamdiuswe@users.noreply.github.com>
2020-09-22 12:12:46 +0600

    Update missing data.py

--- 0.data_preprocessing/missing data.py
+++ 0.data_preprocessing/missing data.py
@@ -12,18 +12,23 @@ y = dataset.iloc[:,[3]].values
 
 
 #Taking care of missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN',strategy = 'mean',axis = 0)
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
 imputer.fit(X[:,1:3])
-X[:,1:3] = imputer.transform(X[:,1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 #Encoding the data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X =  LabelEncoder()
-X[:,0] = labelencoder_X.fit_transform(X[:,0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(
+    transformers = [
+        ('encoder',OneHotEncoder(),[0])
+    ],
+    remainder='passthrough'
+)
+X = np.array(ct.fit_transform(X))
 
+from sklearn.preprocessing import LabelEncoder
 labelencoder_y =  LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
